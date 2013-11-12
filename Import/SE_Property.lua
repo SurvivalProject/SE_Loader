cc00f684880002f87f93d4f2fcf8752c8ae12266
jobro13 <jochembrouwer@outlook.com>
2013-11-12 20:40:23 +0100

    Property Inheritance
    Inheritance for SE Properties!

--- Import/SE_Property.lua
+++ Import/SE_Property.lua
@@ -6,27 +6,44 @@ local DefaultProperty = {}
 
 function DefaultProperty:Clone()
 	local New = CreateProperty(self.PropertyName)
-	for i,v in pairs(self) do 
+	for i,v in pairs(self) do
 		New[i] = v
 	end
-	return New 
+	return New
 end
 
 function PropertyMeta:__index(Index)
-	return PropertyLibrary[self.PropertyName][Index] or DefaultProperty[Index]
-end 
+	return rawget(PropertyLibrary[rawget(self, "PropertyName")],Index) or rawget(PropertyLibrary[rawget(self, "PropertyName")],"Extends") and PropertyLibrary[rawget(PropertyLibrary[rawget(self, "PropertyName")],"Extends")][Index]
+end
+
+function PropertyMeta:__newindex(Value, Index)
+	if Value == "PropertyName" or Value == "Extends" then
+		-- No, no, no.
+		return
+	else
+	rawset(self, Value, Index)
+end
+
+PropertyMeta.__metatable = true -- No, we dont want you messing with our metatables!
 
 function RegisterNewProperty(PropertyName, PropertyData)
 	PropertyData.Type = "Property"
 	PropertyData.PropertyName = PropertyName
-	PropertyLibrary[PropertyName] = PropertyData 
+	if not PropertyData.Extends and PropertyName ~= "DefaultProperty" then
+		PropertyData.Extends = "DefaultProperty"
+	end
+	if type(PropertyData.Extends) == "table" then
+		PropertyData.Extends = PropertyData.Extends.PropertyName
+	end
+	setmetatable(PropertyData, PropertyMeta)
+	PropertyLibrary[PropertyName] = PropertyData
 end
+
+RegisterNewProperty("DefaultProperty", DefaultProperty)
 
 function CreateProperty(PropertyName)
 	local Property = {}
-	Property.PropertyName = PropertyType
+	Property.PropertyName = PropertyName
 	Property.Type = "Property"
 	return setmetatable(Property, PropertyMeta)
-end 
-
-
+end
