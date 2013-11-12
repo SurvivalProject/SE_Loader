d04679f8b83b5bf6c64a5c0b81dd9ec38d380774
jobro13 <jochembrouwer@outlook.com>
2013-11-12 21:27:11 +0100

    SE_Enum cleanup - better system
    Removed unused functions, used general SE style to code it.

--- Import/SE_Enum.lua
+++ Import/SE_Enum.lua
@@ -1,58 +1,44 @@
--- TODO
--- Unuglyfy code
--- CAMELCASE
+local SE_EnumBase = {}
 
-SE_Enum = {}
+local EnumMetatable = {} -- Basic redirects
 
-local Meta = {}
-Meta.__index = function(source, index)
-	return Meta[index]
+function EnumMetatable:__tostring()
+	return self.Name
 end
 
-Meta.__newindex = function(source, index, value)
-	if Meta[index] then
-		-- already exists		
-	else
-		Meta[index]= value
-	end
-end
-
-setmetatable(SE_Enum, Meta)
-
-local enum_meta = {}
-
-function enum_meta:IsEqual(to)
-	return self == to or self.name == to
-end
-
-enum_meta.__newindex = function(source, index, value)
-	return
-end
-enum_meta.__index = function(source, index)
-	return enum_meta[index]
-end
-	
-enum_meta.__tostring = function(source)
-	return source.name
-end
-
-
-local function make(enum, name)
-	local enum = {name = name}
-return	setmetatable(enum, enum_meta)
-end
-	
-
-function SE_Enum.make(enum_name, enum_data)
-	if type(enum_name) == "string" and type(enum_data) == "table" then
-		SE_Enum[enum_name] = {}
-		for i,v in pairs(enum_data) do
-			SE_Enum[enum_name][v] = make(enum_data, v)
+function CreateEnum(EnumName, EnumData)
+	if type(EnumName) == "string" and type(EnumData) == "table" then
+		SE_EnumBase[EnumName] = {}
+		for i,v in pairs(EnumData) do
+			local new = {Name = v, Type = "SE_Enum"}
+			setmetatable(new, EnumMetatable)
+			SE_EnumBase[EnumName][v] = new
 		end
 	end
 end
 
--- How to make an enum:
--- local enum = {"Sine", "Cosine"}
---SE_Enum.make("Sines", enum)
--- index: SE_Enum.Sines.Sine
+SE_Enum = {}
+
+-- Yay redirect functions
+
+local Tracer = {} -- Enum tracer function - temporary
+Tracer.__metatable = true -- NO we don't want you to getmetatable
+function Tracer:__index(Index)
+	return SE_EnumBase[self.root][Index]
+end
+
+function Tracer:__newindex()
+	return
+end
+
+local Meta = {}
+function Meta:__index(Index)
+	local Get = SE_EnumBase[Index]
+	local ETrace = {root=Index}
+	return setmetatable(ETrace, Tracer)
+end
+
+Meta.__metatable = true
+Meta.__newindex = function() return end
+
+setmetatable(SE_Enum, Meta)
