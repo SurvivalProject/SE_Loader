404baa57182788d248b0912fe89c7dae09c12080
MemoryPenguin <memorypenguin@gmail.com>
2013-11-17 14:31:13 -0500

    Updating SE_Import
    Supports loads from ServerStorage or ServerScriptService

--- Import/SE_Import.lua
+++ Import/SE_Import.lua
@@ -4,7 +4,7 @@ function Import(Name)
 		return
 	end	
 
-	local function GetSearchRoot()
+	local function GetSearchRootLighting()
 		local root = game.Lighting.Include[CallFrom]
 		local find = Name
 		for match in string.gmatch(Name, "[^/]+") do
@@ -16,8 +16,40 @@ function Import(Name)
 		end
 		return root
 	end
+
+	local function GetSearchRootServerStorage()
+		local root = game.ServerStorage.Include[CallFrom]
+		local find = Name
+		for match in string.gmatch(Name, "[^/]+") do
+			Name = match
+			root = root:FindFirstChild(match)
+			if not root then
+				return
+			end
+		end
+		return root
+	end
+
+	local function GetSearchRootServerScriptService()
+		local root = game.ServerScriptService.Include[CallFrom]
+		local find = Name
+		for match in string.gmatch(Name, "[^/]+") do
+			Name = match
+			root = root:FindFirstChild(match)
+			if not root then
+				return
+			end
+		end
+		return root
+	end
 		
-	local root = GetSearchRoot()
+	local root = GetSearchRootLighting()
+	if not root then
+		root = GetSearchRootServerStorage()
+	end
+	if not root then
+		root = GetSearchRootServerScriptService()
+	end
 	
 	if root then 
 		local Function, Data = loadstring(root.Value)
