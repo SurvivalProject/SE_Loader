27ab6bf07a5e54aa4fb8a5d797aad196c02d7caa
MemoryPenguin <memorypenguin@gmail.com>
2013-11-17 14:09:29 -0500

    Updated ServerLoader
    Supports the new changes to the plugin and attempts to load from
    ServerScriptService and ServerStorage as well as Lighting.

--- Utilities/ServerLoader.lua
+++ Utilities/ServerLoader.lua
@@ -9,7 +9,23 @@ function Import(Name)
 			return
 		end
 		Function()
-	end	
+	elseif game.ServerStorage:FindFirstChild("Include") and game.ServerStorage.Include:FindFirstChild(CallFrom) and game.ServerStorage.Include[CallFrom]:FindFirstChild(Name) then 
+		local Function, Data = loadstring(game.ServerStorage.Include[CallFrom][Name].Value)
+		if Data then 
+			print("There was an error loading "..Name.."!")
+			print(Data)
+			return
+		end
+		Function()
+	elseif game.ServerScriptService:FindFirstChild("Include") and game.ServerScriptService.Include:FindFirstChild(CallFrom) and game.ServerScriptService.Include[CallFrom]:FindFirstChild(Name) then 
+		local Function, Data = loadstring(game.ServerScriptService.Include[CallFrom][Name].Value)
+		if Data then 
+			print("There was an error loading "..Name.."!")
+			print(Data)
+			return
+		end
+		Function()
+	end		
 end
 
 Import "SE_Server"
