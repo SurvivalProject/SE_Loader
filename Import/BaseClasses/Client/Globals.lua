a7ff5577746ed65c2bf3f212169659d826b434d6
jobro13 <jochembrouwer@outlook.com>
2013-11-21 23:04:14 +0100

    Globals.lua / Client-specific classes or code

---
+++ Import/BaseClasses/Client/Globals.lua
@@ -0,0 +1,9 @@
+-- This file is handy to define global variables for all threads at once!
+Player = game.Players.LocalPlayer
+Camera = game.Workspace.CurrentCamera
+sin = math.sin
+cos = math.cos
+tan = math.tan
+Backpack = Player.Backpack
+PlayerGui = Player:WaitForChild("PlayerGui")
+Mouse = Player:GetMouse()
