13c10ae4de6573b35965244f253e20f5ca28b979
Fattycat17 <munim.rah123456@gmail.com>
2013-12-08 14:02:00 -0500

    Create Camera.lua

---
+++ Import/BaseClasses/Client/Camera.lua
@@ -0,0 +1,12 @@
+local Cam={}
+local Camera=Workspace.Camera
+function Cam:GetRotation()
+	local x,y,z=Camera.CoordinateFrame:toEulerAnglesXYZ()
+	return CFrame.Angles(x,y,z)
+end
+function Cam:GetZoomFocus()
+	return (Camera.Focus.p-Camera.CoordinateFrame.p).magnitude
+end
+
+
+--Fattycat:  I will be adding more functions in a bit just wanted to start this
